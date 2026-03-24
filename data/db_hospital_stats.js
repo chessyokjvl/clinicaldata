18d397c643b9e3f0d641ec3088c7583f44c5b44d
chessyokjvl <qualityjvl@gmail.com>
2026-03-24 12:02:48 +0700

    Create db_hospital_stats.js

---
+++ data/db_hospital_stats.js
@@ -0,0 +1,19 @@
+// data/db_hospital_stats.js
+
+const hospitalOverallStats = [
+    { year: "2555", opd: 33507, ipd: 1418, alos: 22.03, occRate: 71.12 },
+    { year: "2556", opd: 34659, ipd: 1570, alos: 25.35, occRate: 90.86 },
+    { year: "2557", opd: 35894, ipd: 1901, alos: 22.93, occRate: 99.51 },
+    { year: "2558", opd: 36649, ipd: 2471, alos: 20.87, occRate: 117.76 },
+    { year: "2559", opd: 35184, ipd: 2401, alos: 21.45, occRate: 117.25 },
+    { year: "2560", opd: 33902, ipd: 2523, alos: 21.99, occRate: 126.68 },
+    { year: "2561", opd: 35618, ipd: 2506, alos: 25.38, occRate: 145.21 },
+    { year: "2562", opd: 37106, ipd: 2496, alos: 22.56, occRate: 128.59 },
+    { year: "2563", opd: 34067, ipd: 2352, alos: 21.88, occRate: 117.20 },
+    { year: "2564", opd: 36809, ipd: 2299, alos: 20.76, occRate: 108.99 },
+    { year: "2565", opd: 40964, ipd: 1777, alos: 19.23, occRate: 78.01 },
+    { year: "2566", opd: 42425, ipd: 2280, alos: 17.33, occRate: 90.22 },
+    { year: "2567", opd: 45415, ipd: 2487, alos: 18.03, occRate: 102.12 },
+    { year: "2568", opd: 43930, ipd: 2275, alos: 18.47, occRate: 95.95 },
+    { year: "2569 (3 เดือน)", opd: 11099, ipd: 416, alos: 20.82, occRate: 78.46 }
+];
